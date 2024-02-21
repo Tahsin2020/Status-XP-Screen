cd9694ce10feb412ce8d1456b426e131b226e0a4
Tahsin Hasan <tahsinhasan2@gmail.com>
2024-02-21 11:05:10 -0800

    Initial Skill Table created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,52 @@
 import useWindowDimensions from "./components/windowDimensions";
+
 import "./App.css";
 
 const App = () => {
   const { height, width } = useWindowDimensions();
+  var skills = {
+    "Meditation Breathing": 2,
+    "Meditation Sensing": 3,
+    "Meditation Duration": 1,
+    Determination: 0,
+    Javascript: 1,
+  };
 
   return (
     <div style={{ height: "100%" }}>
+      <div className="Prompt">
+        <div className="Prompt-Box">
+          Hi
+        </div>
+        <div className="Prompt-Background"/>
+      </div>
       {width} by {height} pixels
       <div className="skill-table">
+        {Object.keys(skills).map((key: String) => {
+          return (
+            <div className="skill">
+              <div className="skill-name">{key}</div>
+              <div className="skill-bar" />
+              <div className="skill-level">{skills[key]}</div>
+              <div className="skill-up">+</div>
+              <div className="skill-down">-</div>
+              <div className="skill-delete">x</div>
+            </div>
+          );
+        })}
         <div className="skill">
-          <div className="skill-name">HTMLadasdsadasasdasdasdasdsa</div>
-          <div className="skill-bar"></div>
-        </div>
-        <div className="skill">
-          <div className="skill-name">CSS</div>
-          <div className="skill-bar"></div>
-        </div>
-        <div className="skill">
-          <div className="skill-name">JS</div>
-          <div className="skill-bar"></div>
-        </div>
-        <div className="skill">
-          <div className="skill-name">JS</div>
-          <div className="skill-bar"></div>
-        </div>
-        <div className="skill">
-          <div className="skill-name">JS</div>
-          <div className="skill-bar"></div>
+          <button
+            style={{
+              backgroundColor:"skyblue",
+              color:"white",
+              borderRadius: "10px",
+              width: "400px",
+              fontSize: "24px",
+              padding: "10px",
+            }}
+          >
+            Add Skill?
+          </button>
         </div>
       </div>
     </div>
